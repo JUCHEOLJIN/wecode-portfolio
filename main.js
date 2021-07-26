4e0893d20b27e2d28ad1eb75896ac0fc45294d51
jin <toto006521@gmail.com>
2021-07-26 18:03:44 +0900

    Add cursor event

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
 const toggleBtn = document.querySelector(".header__toggle");
 const menu = document.querySelector(".header__menu");
 const item = document.querySelector(".header__item");
+const cursor = document.querySelector(".cursor");
 
 toggleBtn.addEventListener("click", () => {
   menu.classList.toggle("active");
@@ -13,4 +14,34 @@ menu.addEventListener("click", (event) => {
   if (event.target.matches("a")) {
     menu.classList.toggle("active");
   }
+});
+
+addEventListener("load", (event) => {
+  const cursorRect = cursor.getBoundingClientRect();
+  const cursorHalfWidth = cursorRect.width / 2;
+  const cursorHalfHeight = cursorRect.height / 2;
+
+  document.addEventListener("mousemove", (event) => {
+    let X = event.clientX;
+    let Y = event.clientY;
+    cursor.style.transform = `translate(${X - cursorHalfWidth}px, ${
+      Y - cursorHalfHeight
+    }px)`;
+  });
+});
+
+menu.addEventListener("mouseover", (event) => {
+  cursor.classList.add("cursor--grow");
+});
+
+menu.addEventListener("mouseleave", (event) => {
+  cursor.classList.remove("cursor--grow");
+});
+
+toggleBtn.addEventListener("mouseover", () => {
+  cursor.classList.add("cursor--grow");
+});
+
+toggleBtn.addEventListener("mouseleave", () => {
+  cursor.classList.remove("cursor--grow");
 });
